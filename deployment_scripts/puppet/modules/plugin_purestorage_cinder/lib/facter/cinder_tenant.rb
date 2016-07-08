34f976506ea40c17253c6a90e1f7d8e1cfc1f033
Simon Dodsley <simon@purestorage.com>
2016-07-08 14:50:24 -0400

    Cleanup unneccessary files
    Change-Id: I154e9401a53bf3499e4f496622d01a0c218f2932

--- deployment_scripts/puppet/modules/plugin_purestorage_cinder/lib/facter/cinder_tenant.rb
+++
@@ -1,7 +0,0 @@
-require 'facter/util/resolution'
-
-Facter.add("cinder_tenant") do
-  setcode do
-    tenant = Facter::Util::Resolution.exec("/usr/bin/openstack project show cinder_internal_tenant | grep id | awk \'{print $4}\'")
-  end
-end
